c10ad461c4ef49d57af24646f17f4dda69ac4c5e
Rostislav Blaha <Rostislav Blaha>
2016-10-01 11:56:00 +0200

    Grid rendering moved to a separate component.

---
+++ src/Grid.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export default class Grid extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {grid:""}  
+    }
+	
+	componentDidMount(){
+        this.setState({grid: this.drawSquareGrid(5, 210, 297, "silver")})
+    }
+	
+	drawSquareGrid(distance, width, height, colour){
+        var horizontalLength = Math.floor(width/distance)
+        var verticalLength = Math.floor(height/distance)
+        var size = 0.3
+        var grid = []
+
+        var posX = size
+        var posY = size
+        
+        for (let k = 0; k < verticalLength+1; k++) { 
+            for (let i = 0; i < horizontalLength+1; i++) { 
+                var c = <circle  key={grid.length}  cx={posX + "mm"} cy={posY + "mm"} r={size + "mm"} fill={colour}/>
+                    grid[grid.length] = c
+                    posX += (distance-size)
+            }
+            console.log("radek")
+            posX = size
+            posY += distance       
+        }
+        
+        return grid
+        
+    }
+ 
+  render() {
+  	var grid = this.state.grid
+    return (
+	 	<div>
+			<svg width="210mm" height="290mm">
+					{grid}
+			</svg>
+		</div>
+	)
+  }
+}
