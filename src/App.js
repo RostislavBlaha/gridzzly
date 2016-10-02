aa01639cd8b0289e93b6c786711162efbb6c8d76
Rostislav Blaha <Rostislav Blaha>
2016-10-02 11:58:31 +0200

    Work on ruler component started.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Grid from './Grid'
+import Ruler from './Ruler'
 
 class App extends Component {
     
@@ -10,7 +11,8 @@ class App extends Component {
     
   render() {
     return (
-      <div className="App">  
+      <div className="App">
+		<Ruler/>
 		<Grid 	width=""
 				height=""
 				distance=""
---
+++ src/Ruler.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Ruler extends Component{
+
+ 
+  render() {
+	  const sx = {
+		width: "100%",
+		height: "100px",
+		backgroundColor: "#ffd633",
+	  }
+    return (
+	 	<div style={sx}>
+		</div>
+	)
+  }
+}
